77511313d751947b2f220e02d3a7a6f73aea52d2
Tommy <tommy45344@gmail.com>
2020-08-28 17:10:12 +1000

    Update to project - finshed the core of the todo app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "clean up room",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "meal prep",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "go to gym",
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  toggleComplete = (id) =>{
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  //Delete Todo
+  delTodo = (id) =>{
+    this.setState({todos: 
+      [...this.state.todos.filter(todo => todo.id !== id)] 
+      });
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} 
+        delTodo={this.delTodo}/>
+      </div>
+    )
+  }
 }
 
 export default App;
